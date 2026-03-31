3dc511de6b7377eee330230040814f896f89d1ed
Gosxa <ihorkondrashov@gmail.com>
2026-03-31 19:07:39 +0300

    terraform task5

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  # Конфигурация удаленного бэкенда
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "gosxanstorage369"
+    container_name       = "tfstate"             # Имя контейнера внутри Storage Account
+    key                  = "terraform.tfstate"   # Имя файла стейта в контейнере
+    use_oidc             = true                  # Включаем OIDC для бэкенда
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true # Включаем OIDC для провайдера
+}
